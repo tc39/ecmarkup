3d1acdf6b1e7d4f1cb7404fb7c47c58a21b8a104
Brian Terlson <brian.terlson@microsoft.com>
2015-05-23 19:02:44 -0700

    Attempt to fix travis build

--- test/output.js
+++ test/output.js
@@ -12,7 +12,8 @@ describe('output of test.html', function() {
     return build(testPath, function(file) {
       return readFile(file, 'utf-8');
     }).then(function(contents) {
-      assert.equal(contents, fs.readFileSync(testPath + '.baseline', 'utf-8'));
+      assert.equal(contents,
+          fs.readFileSync(testPath + '.baseline', 'utf-8').toString());
     });
   });
 });
