9e9b6c8ffdcc01dd53a3e7a8fade567e638c1eec
Brian Terlson <brian.terlson@microsoft.com>
2015-05-23 19:09:35 -0700

    Diff output for test failures

--- test/output.js
+++ test/output.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var assert = require('assert');
 var Promise = require('bluebird');
 var readFile = Promise.promisify(fs.readFile);
+var diff = require('diff');
 
 var build = require('../lib/ecmarkup').build;
 
@@ -12,8 +13,10 @@ describe('output of test.html', function() {
     return build(testPath, function(file) {
       return readFile(file, 'utf-8');
     }).then(function(contents) {
-      assert.equal(contents,
-          fs.readFileSync(testPath + '.baseline', 'utf-8').toString());
+      var str = fs.readFileSync(testPath + '.baseline', 'utf-8').toString() + "a";
+      if(contents !== str) {
+        throw new Error(diff.createPatch("test.html", contents, str))
+      }
     });
   });
 });
