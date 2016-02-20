55461f5be839ef1c5d8f4e5c3ff3d55e5cc9af77
Brian Terlson <brian.terlson@microsoft.com>
2016-02-19 17:32:09 -0800

    Add namespace attribute to clauses.

---
+++ test/biblio.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const assert = require('assert');
+const Biblio = require('../lib/Biblio');
+const location = 'https://tc39.github.io/ecma262/';
+
+describe('Biblio', function () {
+  let biblio;
+
+  beforeEach(function() {
+    biblio = new Biblio(location);
+  });
+
+  specify('is created with a root namespace named "global"', function () {
+    const opEntry = {
+      type: 'op',
+      aoid: 'aoid',
+      refId: 'clauseId'
+    };
+
+    biblio.add(opEntry, 'global');
+    assert.equal(biblio.byAoid('aoid'), opEntry);
+  });
+
+  specify('is created with a nested namespace for the doc\'s location', function () {
+    const opEntry = {
+      type: 'op',
+      aoid: 'aoid',
+      refId: 'clauseId'
+    };
+
+    biblio.add(opEntry, location);
+    assert.equal(biblio.byAoid('aoid', 'global'), undefined);
+    assert.equal(biblio.byAoid('aoid', location), opEntry);
+  });
+
+  specify('stringifies with only the local scope', function () {
+    const opEntry1 = {
+      type: 'op',
+      aoid: 'aoid1',
+      refId: 'clauseId'
+    };
+
+    const opEntry2 = {
+      type: 'op',
+      aoid: 'aoid2',
+      refId: 'clauseId'
+    };
+
+    biblio.add(opEntry1, location);
+    biblio.add(opEntry2, 'global');
+
+    const str = JSON.stringify(biblio);
+
+    assert(str.indexOf('aoid1') > -1);
+    assert(str.indexOf('aoid2') === -1);
+  });
+
+  describe('inScopeByType', function () {
+    specify('de-dupes by key', function () {
+      const opEntry1 = {
+        type: 'op',
+        aoid: 'aoid',
+        refId: 'clauseId'
+      };
+
+      const opEntry2 = {
+        type: 'op',
+        aoid: 'aoid',
+        refId: 'clauseId'
+      };
+
+      biblio.add(opEntry1, location);
+      biblio.add(opEntry2, 'global');
+
+      let results = biblio.inScopeByType(location, 'op');
+      assert.equal(results[0], opEntry1);
+    });
+  });
+});
