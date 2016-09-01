6392279bc521e97ad2f9d86ef2547cd74db73766
Ron Buckton <rbuckton@microsoft.com>
2016-09-01 11:45:21 -0700

    Simplified coroutine call.

--- src/awaiter.ts
+++ src/awaiter.ts
@@ -1,6 +1,4 @@
 import Promise = require("bluebird");
 export = function (thisArg: any, _arguments: any, P: PromiseConstructorLike, generator: any) {
-  return (<any>Promise).coroutine(generator, {
-    yieldHandler(value: any) { return Promise.cast(value); }
-  }).apply(thisArg, _arguments);
+  return (<any>Promise).coroutine(generator, { yieldHandler: Promise.cast }).apply(thisArg, _arguments);
 };
