662f3a2494485c05ad679969635d910a08c23a6c
Brian Terlson <brian.terlson@microsoft.com>
2016-09-02 11:41:30 -0700

    Add rationale to awaiter.ts

--- src/awaiter.ts
+++ src/awaiter.ts
@@ -5,6 +5,8 @@ import Promise = require("bluebird");
  * use custom helpers to override TypeScript runtime behavior in a compatible fashion.
  *
  * https://github.com/Microsoft/TypeScript/issues/6737#issuecomment-177007790
+ * 
+ * We use a custom __awaiter for performance reasons - bluebird is still faster than native promises.
  */
 export = function (thisArg: any, _arguments: any, P: PromiseConstructorLike, generator: any) {
   return (<any>Promise).coroutine(generator, { yieldHandler: Promise.cast }).apply(thisArg, _arguments);
