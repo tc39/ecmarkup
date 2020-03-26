b741249c73c21ca3d57cdf32d7f44b011c1c652d
Kevin Gibbons <bakkot@gmail.com>
2020-03-26 08:59:07 -0700

    Fail the process on build error

---
+++ test/cli.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+const execSync = require('child_process').execSync;
+
+describe('ecmarkup#cli', () => {
+  it('exits cleanly on success', () => {
+    execSync('./bin/ecmarkup.js test/example.html', { encoding: 'utf8' });
+  });
+
+  it('exits with an error on error', () => {
+    assert.throws(() => {
+      execSync('./bin/ecmarkup.js test/malformed.html', { encoding: 'utf8' });
+    });
+  });
+});
