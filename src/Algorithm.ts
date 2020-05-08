7b97deb8a567f8006338f7a0abc2ec9a4c3bf315
Kevin Gibbons <bakkot@gmail.com>
2020-05-08 15:42:50 -0700

    remove some now-unnecessary special casing

--- src/Algorithm.ts
+++ src/Algorithm.ts
@@ -2,21 +2,12 @@ import type { Context } from './Context';
 
 import Builder from './Builder';
 import * as emd from 'ecmarkdown';
-import Grammar from './Grammar';
 
 /*@internal*/
 export default class Algorithm extends Builder {
   static enter(context: Context) {
     const { node } = context;
-    // Need to process emu-grammar elements first so emd doesn't trash the syntax inside emu-grammar nodes
-    const grammarNodes = node.querySelectorAll('emu-grammar');
-    for (let i = 0; i < grammarNodes.length; i++) {
-      let gn = grammarNodes[i];
-      context.node = gn as HTMLElement;
-      Grammar.enter(context);
-      Grammar.exit(context);
-      context.node = node;
-    }
+
     // replace spaces after !/? with &nbsp; to prevent bad line breaking
     const html = emd
       .algorithm(node.innerHTML)
