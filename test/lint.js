807c39b66c188568b2431970f2cbc49ea7a399d8
Kevin Gibbons <kevin@shapesecurity.com>
2020-05-14 20:20:10 -0700

    add negative test

--- test/lint.js
+++ test/lint.js
@@ -139,35 +139,71 @@ describe('linting', function () {
         'expected "If" with substeps to end with ", then" (found ",")'
       );
     });
+
+    it('negative', async function () {
+      await assertLint(`
+        <emu-alg>
+          1. If foo, bar.
+          1. Else if foo, bar.
+          1. Else, bar.
+          1. If foo, then
+            1. Substep.
+          1. Else if foo, then
+            1. Substep.
+          1. Else,
+            1. Substep.
+          1. Repeat,
+            1. Substep.
+          1. Repeat, while foo,
+            1. Substep.
+          1. Repeat, until foo,
+            1. Substep.
+          1. For each foo, do bar.
+          1. For each foo, do
+            1. Substep.
+          1. Other.
+          1. Other:
+            1. Substep.
+        </emu-alg>
+      `);
+    });
+
   });
 });
 
-async function assertLint(src, message) {
-  let line, column;
-  src.split('\n').forEach((contents, lineNo) => {
-    let idx = contents.indexOf(M);
-    if (idx !== -1) {
-      line = lineNo + 1;
-      column = idx + 1;
-    }
-  });
-  let offset = src.indexOf(M);
-  src = src.substring(0, offset) + src.substring(offset + M.length);
+async function assertLint(src, message = null) {
+  let reportLintErrors;
+  if (message === null) {
+    reportLintErrors = (errors) => {
+      throw new Error('unexpected errors ' + JSON.stringify(errors));
+    };
+  } else {
+    let line, column;
+    src.split('\n').forEach((contents, lineNo) => {
+      let idx = contents.indexOf(M);
+      if (idx !== -1) {
+        line = lineNo + 1;
+        column = idx + 1;
+      }
+    });
+    let offset = src.indexOf(M);
+    src = src.substring(0, offset) + src.substring(offset + M.length);
+    reportLintErrors = (errors) => {
+      reported = true;
+      assert.equal(errors.length, 1, 'should have exactly one error');
+      assert.deepStrictEqual(errors[0], {
+        line,
+        column,
+        message,
+      });
+    };
+  }
 
   let reported = false;
-  function reportLintErrors(errors) {
-    reported = true;
-    assert.equal(errors.length, 1, 'should have exactly one error');
-    assert.deepStrictEqual(errors[0], {
-      line,
-      column,
-      message,
-    });
-  }
   await emu.build('test-example.emu', async () => src, {
     ecma262Biblio: false,
     copyright: false,
     reportLintErrors,
   });
-  assert(reported);
+  assert.equal(reported, message !== null);
 }
