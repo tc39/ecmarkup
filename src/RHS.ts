cdd3a85247babc7af990c3b55da555a0f4f294ec
Kevin Gibbons <bakkot@gmail.com>
2020-09-05 18:40:05 -0700

    support ins/del in emu-production tags

--- src/RHS.ts
+++ src/RHS.ts
@@ -35,25 +35,43 @@ export default class RHS extends Builder {
   }
 
   terminalify(parentNode: Element) {
+    // we store effects to perform later so the iteration doesn't get messed up
+    let pairs: { parent: Element; child: Text }[] = [];
     for (let i = 0; i < parentNode.childNodes.length; i++) {
       const node = parentNode.childNodes[i];
-
       if (node.nodeType === 3) {
-        const text = node.textContent!.trim();
-        const pieces = text.split(/\s/);
-
-        pieces.forEach(p => {
-          if (p.length === 0) {
-            return;
+        pairs.push({ parent: parentNode, child: node as Text });
+      } else if (
+        node.nodeType === 1 &&
+        (node.nodeName === 'INS' || node.nodeName === 'DEL' || node.nodeName === 'MARK')
+      ) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+          const child = node.childNodes[i];
+          if (child.nodeType === 3) {
+            pairs.push({ parent: node as Element, child: child as Text });
           }
-          const est = this.spec.doc.createElement('emu-t');
-          est.textContent = p;
-
-          parentNode.insertBefore(est, node);
-        });
-
-        parentNode.removeChild(node);
+        }
       }
     }
+    for (let { parent, child } of pairs) {
+      this.wrapTerminal(parent, child);
+    }
+  }
+
+  private wrapTerminal(parentNode: Element, node: Text) {
+    const text = node.textContent!.trim();
+    const pieces = text.split(/\s/);
+
+    pieces.forEach(p => {
+      if (p.length === 0) {
+        return;
+      }
+      const est = this.spec.doc.createElement('emu-t');
+      est.textContent = p;
+
+      parentNode.insertBefore(est, node);
+    });
+
+    parentNode.removeChild(node);
   }
 }
