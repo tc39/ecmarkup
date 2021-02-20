3dbc8d4ffe137d396859ee31a7cebd62add92491
Kevin Gibbons <bakkot@gmail.com>
2021-02-20 12:27:49 -0800

    auto-fix js assets

--- js/sdoMap.js
+++ js/sdoMap.js
@@ -1,6 +1,5 @@
 'use strict';
-
-var sdoBox = {
+let sdoBox = {
   init: function () {
     this.$alternativeId = null;
     this.$outer = document.createElement('div');
@@ -10,14 +9,11 @@ var sdoBox = {
     this.$displayLink = document.createElement('a');
     this.$displayLink.setAttribute('href', '#');
     this.$displayLink.textContent = 'Syntax-Directed Operations';
-    this.$displayLink.addEventListener(
-      'click',
-      function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        referencePane.showSDOs(sdoMap[this.$alternativeId] || {}, this.$alternativeId);
-      }.bind(this)
-    );
+    this.$displayLink.addEventListener('click', e => {
+      e.preventDefault();
+      e.stopPropagation();
+      referencePane.showSDOs(sdoMap[this.$alternativeId] || {}, this.$alternativeId);
+    });
     this.$container.appendChild(this.$displayLink);
     this.$outer.appendChild(this.$container);
     document.body.appendChild(this.$outer);
@@ -27,11 +23,11 @@ var sdoBox = {
     clearTimeout(this.deactiveTimeout);
     Toolbox.deactivate();
     this.$alternativeId = el.id;
-    var numSdos = Object.keys(sdoMap[this.$alternativeId] || {}).length;
+    let numSdos = Object.keys(sdoMap[this.$alternativeId] || {}).length;
     this.$displayLink.textContent = 'Syntax-Directed Operations (' + numSdos + ')';
     this.$outer.classList.add('active');
-    var top = el.offsetTop - this.$outer.offsetHeight;
-    var left = el.offsetLeft + 50 - 10; // 50px = padding-left(=75px) + text-indent(=-25px)
+    let top = el.offsetTop - this.$outer.offsetHeight;
+    let left = el.offsetLeft + 50 - 10; // 50px = padding-left(=75px) + text-indent(=-25px)
     this.$outer.setAttribute('style', 'left: ' + left + 'px; top: ' + top + 'px');
     if (top < document.body.scrollTop) {
       this.$container.scrollIntoView();
@@ -44,28 +40,26 @@ var sdoBox = {
   },
 };
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   sdoBox.init();
 
-  var insideTooltip = false;
-  sdoBox.$outer.addEventListener('pointerenter', function () {
+  let insideTooltip = false;
+  sdoBox.$outer.addEventListener('pointerenter', () => {
     insideTooltip = true;
   });
-  sdoBox.$outer.addEventListener('pointerleave', function () {
+  sdoBox.$outer.addEventListener('pointerleave', () => {
     insideTooltip = false;
     sdoBox.deactivate();
   });
 
   sdoBox.deactiveTimeout = null;
-  [].forEach.call(document.querySelectorAll('emu-grammar[type=definition] emu-rhs'), function (
-    node
-  ) {
+  [].forEach.call(document.querySelectorAll('emu-grammar[type=definition] emu-rhs'), node => {
     node.addEventListener('pointerenter', function () {
       sdoBox.activate(this);
     });
 
-    node.addEventListener('pointerleave', function (e) {
-      sdoBox.deactiveTimeout = setTimeout(function () {
+    node.addEventListener('pointerleave', e => {
+      sdoBox.deactiveTimeout = setTimeout(() => {
         if (!insideTooltip) {
           sdoBox.deactivate();
         }
@@ -75,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.addEventListener(
     'keydown',
-    debounce(function (e) {
+    debounce(e => {
       if (e.code === 'Escape') {
         sdoBox.deactivate();
       }
@@ -84,8 +78,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var sdoMap = {};
-document.addEventListener('DOMContentLoaded', function () {
-  var sdoMapContainer = document.getElementById('sdo-map');
+document.addEventListener('DOMContentLoaded', () => {
+  let sdoMapContainer = document.getElementById('sdo-map');
   if (sdoMapContainer == null) {
     console.error('could not find SDO map container');
   } else {
