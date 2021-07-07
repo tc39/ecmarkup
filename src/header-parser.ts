215c6af23ad65318e5fbb2b7891319a815e529a6
Kevin Gibbons <bakkot@gmail.com>
2021-07-07 13:07:04 -0700

    Support for parsing structured headers for AOs and similar operations (#327)

---
+++ src/header-parser.ts
@@ -0,0 +1,377 @@
+import type Spec from './Spec';
+import { offsetToLineAndColumn } from './utils';
+
+export function parseStructuredHeaderH1(
+  spec: Spec,
+  header: Element
+): { name: string | null; formattedHeader: string | null; formattedParams: string | null } {
+  // parsing is intentionally permissive; the linter can do stricter checks
+  // TODO have the linter do checks
+  let headerText = header.innerHTML;
+  const prefix = headerText.match(/^\s*(Static|Runtime) Semantics:\s*/);
+  if (prefix != null) {
+    headerText = headerText.substring(prefix[0].length);
+  }
+
+  const parsed = headerText.match(/^(?<beforeParams>\s*(?<name>[^(\s]+)\s*\()(?<params>.*)\)\s*$/s);
+  if (parsed == null) {
+    spec.warn({
+      type: 'contents',
+      ruleId: 'header-format',
+      message: `failed to parse header`,
+      node: header,
+      nodeRelativeColumn: 1,
+      nodeRelativeLine: 1,
+    });
+    return { name: null, formattedHeader: null, formattedParams: null };
+  }
+
+  type Param = { name: string; type: string | null };
+  const name = parsed.groups!.name;
+  let paramText = parsed.groups!.params;
+  const params: Array<Param> = [];
+  const optionalParams: Array<Param> = [];
+  let formattedHeader = null;
+
+  if (/\(\s*\n/.test(headerText)) {
+    // if it's multiline, parse it for types
+    const paramLines = paramText.split('\n');
+    let index = 0;
+    let offset = 0;
+    for (const line of paramLines) {
+      offset += line.length;
+      const chunk = line.trim();
+      if (chunk === '') {
+        continue;
+      }
+      ++index;
+      function getParameterOffset() {
+        return (
+          (prefix?.[0].length ?? 0) +
+          parsed!.groups!.beforeParams.length +
+          (offset - line.length) + // we've already updated offset to include line.length at this point
+          index + // to account for the `\n`s eaten by the .split
+          line.match(/^\s*/)![0].length
+        );
+      }
+      const parsedChunk = chunk.match(/^(optional\s+)?([A-Za-z0-9_]+)\s*:\s*(\S.*\S)/);
+      if (parsedChunk == null) {
+        const { line: nodeRelativeLine, column: nodeRelativeColumn } = offsetToLineAndColumn(
+          header.innerHTML,
+          getParameterOffset()
+        );
+        spec.warn({
+          type: 'contents',
+          ruleId: 'header-format',
+          message: `failed to parse parameter ${index}`,
+          node: header,
+          nodeRelativeColumn,
+          nodeRelativeLine,
+        });
+        continue;
+      }
+      const optional = parsedChunk[1] != null;
+      const paramName = parsedChunk[2];
+      let paramType = parsedChunk[3];
+      if (paramType.endsWith(',')) {
+        paramType = paramType.slice(0, -1);
+      }
+      if (!optional && optionalParams.length > 0) {
+        const { line: nodeRelativeLine, column: nodeRelativeColumn } = offsetToLineAndColumn(
+          header.innerHTML,
+          getParameterOffset()
+        );
+        spec.warn({
+          type: 'contents',
+          ruleId: 'header-format',
+          message: `required parameters should not follow optional parameters`,
+          node: header,
+          nodeRelativeColumn,
+          nodeRelativeLine,
+        });
+      }
+      (optional ? optionalParams : params).push({
+        name: paramName,
+        type: paramType === 'unknown' ? null : paramType,
+      });
+    }
+    const formattedPrefix = prefix == null ? '' : prefix[0].trim() + ' ';
+    formattedHeader = `${formattedPrefix}${name} (${params.map(n => ' ' + n.name).join(',')}`;
+    if (optionalParams.length > 0) {
+      formattedHeader +=
+        optionalParams
+          .map((p, i) => ' [ ' + (i > 0 || params.length > 0 ? ', ' : '') + p.name)
+          .join('') + optionalParams.map(() => ' ]').join('');
+    }
+    formattedHeader += ' )';
+  } else {
+    let optional = false;
+    paramText = paramText.trim();
+    while (true) {
+      if (paramText.length == 0) {
+        break;
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      let { success, text, match } = eat(paramText, /^\s*\[(\s*,)?/);
+      if (success) {
+        optional = true;
+        paramText = text;
+      }
+      ({ success, text, match } = eat(paramText, /^\s*([A-Za-z0-9_]+)/));
+      if (!success) {
+        spec.warn({
+          type: 'contents',
+          ruleId: 'header-format',
+          message: `failed to parse header`,
+          node: header,
+          // we could be more precise, but it's probably not worth the effort
+          nodeRelativeLine: 1,
+          nodeRelativeColumn: 1,
+        });
+        break;
+      }
+      paramText = text;
+      (optional ? optionalParams : params).push({ name: match![1], type: null });
+      ({ success, text } = eat(paramText, /^(\s*\])+|,/));
+      if (success) {
+        paramText = text;
+      }
+    }
+  }
+
+  const printParam = (p: Param) => `${p.name}${p.type == null ? '' : ` (${p.type})`}`;
+  const paramsWithTypes = params.map(printParam);
+  const optionalParamsWithTypes = optionalParams.map(printParam);
+  let formattedParams = '';
+  if (params.length === 0 && optionalParams.length === 0) {
+    formattedParams = 'no arguments';
+  } else {
+    if (params.length > 0) {
+      formattedParams =
+        (params.length === 1 ? 'argument' : 'arguments') + ' ' + formatEnglishList(paramsWithTypes);
+      if (optionalParams.length > 0) {
+        formattedParams += ' and ';
+      }
+    }
+    if (optionalParams.length > 0) {
+      formattedParams +=
+        'optional ' +
+        (optionalParams.length === 1 ? 'argument' : 'arguments') +
+        ' ' +
+        formatEnglishList(optionalParamsWithTypes);
+    }
+  }
+
+  return { name, formattedHeader, formattedParams };
+}
+
+export function parseStructuredHeaderDl(
+  spec: Spec,
+  type: string | null,
+  dl: Element
+): { description: Element | null; for: Element | null } {
+  let description = null;
+  let _for = null;
+  for (let i = 0; i < dl.children.length; ++i) {
+    const dt = dl.children[i];
+    if (dt.tagName !== 'DT') {
+      spec.warn({
+        type: 'node',
+        ruleId: 'header-format',
+        message: `expecting header to have DT, but found ${dt.tagName}`,
+        node: dt,
+      });
+      break;
+    }
+    ++i;
+    const dd = dl.children[i];
+    if (dd?.tagName !== 'DD') {
+      spec.warn({
+        type: 'node',
+        ruleId: 'header-format',
+        message: `expecting header to have DD, but found ${dd.tagName}`,
+        node: dd,
+      });
+      break;
+    }
+
+    const dtype = dt.textContent ?? '';
+    switch (dtype.trim().toLowerCase()) {
+      case 'description': {
+        if (description != null) {
+          spec.warn({
+            type: 'node',
+            ruleId: 'header-format',
+            message: `duplicate "description" attribute`,
+            node: dt,
+          });
+        }
+        description = dd;
+        break;
+      }
+      case 'for': {
+        if (_for != null) {
+          spec.warn({
+            type: 'node',
+            ruleId: 'header-format',
+            message: `duplicate "for" attribute`,
+            node: dt,
+          });
+        }
+        if (type === 'concrete method' || type === 'internal method') {
+          _for = dd;
+        } else {
+          spec.warn({
+            type: 'node',
+            ruleId: 'header-format',
+            message: `"for" attributes only apply to concrete or internal methods`,
+            node: dt,
+          });
+        }
+        break;
+      }
+      case '': {
+        spec.warn({
+          type: 'node',
+          ruleId: 'header-format',
+          message: `missing value for structured header attribute`,
+          node: dt,
+        });
+        break;
+      }
+      default: {
+        spec.warn({
+          type: 'node',
+          ruleId: 'header-format',
+          message: `unknown structured header entry type ${JSON.stringify(dtype)}`,
+          node: dt,
+        });
+        break;
+      }
+    }
+  }
+  return { description, for: _for };
+}
+
+export function formatPreamble(
+  spec: Spec,
+  clause: Element,
+  dl: Element,
+  type: string | null,
+  name: string,
+  formattedParams: string,
+  _for: Element | null,
+  description: Element | null
+): Array<Element> {
+  const para = spec.doc.createElement('p');
+  const paras = [para];
+  switch (type?.toLowerCase()) {
+    case 'numeric method':
+    case 'abstract operation': {
+      // TODO tests (for each type of parametered thing) which have HTML in the parameter type
+      para.innerHTML += `The abstract operation ${name} takes ${formattedParams}.`;
+      break;
+    }
+    case 'host-defined abstract operation': {
+      para.innerHTML += `The host-defined abstract operation ${name} takes ${formattedParams}.`;
+      break;
+    }
+    case 'implementation-defined abstract operation': {
+      para.innerHTML += `The implementation-defined abstract operation ${name} takes ${formattedParams}.`;
+      break;
+    }
+    case 'internal method':
+    case 'concrete method': {
+      if (_for == null) {
+        spec.warn({
+          type: 'contents',
+          ruleId: 'header-format',
+          message: `expected ${type} to have a "for"`,
+          node: dl,
+          nodeRelativeLine: 1,
+          nodeRelativeColumn: 1,
+        });
+        _for = spec.doc.createElement('div');
+      }
+      para.append(
+        `The ${name} ${type} of `,
+        // @ts-ignore childNodes is iterable
+        ..._for.childNodes
+      );
+      para.innerHTML += ` takes ${formattedParams}.`;
+      break;
+    }
+    default: {
+      if (type) {
+        spec.warn({
+          type: 'attr-value',
+          ruleId: 'header-type',
+          message: `unknown clause type ${JSON.stringify(type)}`,
+          node: clause,
+          attr: 'type',
+        });
+      } else {
+        spec.warn({
+          type: 'node',
+          ruleId: 'header-type',
+          message: `clauses with structured headers should have a type`,
+          node: clause,
+        });
+      }
+    }
+  }
+  if (description != null) {
+    // @ts-ignore childNodes is iterable
+    const isJustElements = [...(description.childNodes as Iterable<Node>)].every(
+      n => n.nodeType === 1 || (n.nodeType === 3 && n.textContent?.trim() === '')
+    );
+    if (isJustElements) {
+      // @ts-ignore childNodes is iterable
+      paras.push(...description.childNodes);
+    } else {
+      // @ts-ignore childNodes is iterable
+      para.append(' ', ...description.childNodes);
+    }
+  }
+  let next = dl.nextElementSibling;
+  while (next != null && next.tagName === 'EMU-NOTE') {
+    next = next.nextElementSibling;
+  }
+  if (next?.tagName == 'EMU-ALG' && !next.hasAttribute('replaces-step')) {
+    if (paras.length > 1 || next !== dl.nextElementSibling) {
+      const whitespace = next.previousSibling;
+      const after = spec.doc.createElement('p');
+      after.append('It performs the following steps when called:');
+      next.parentElement!.insertBefore(after, next);
+
+      // fix up the whitespace in the generated HTML
+      if (whitespace?.nodeType === 3 /* TEXT_NODE */ && /^\s+$/.test(whitespace.nodeValue!)) {
+        next.parentElement!.insertBefore(whitespace.cloneNode(), next);
+      }
+    } else {
+      para.append(' It performs the following steps when called:');
+    }
+  }
+  return paras;
+}
+
+function eat(text: string, regex: RegExp) {
+  const match = text.match(regex);
+  if (match == null) {
+    return { success: false, match, text };
+  }
+  return { success: true, match, text: text.substring(match[0].length) };
+}
+
+function formatEnglishList(list: Array<string>) {
+  if (list.length === 0) {
+    throw new Error('formatEnglishList should not be called with an empty list');
+  }
+  if (list.length === 1) {
+    return list[0];
+  }
+  if (list.length === 2) {
+    return `${list[0]} and ${list[1]}`;
+  }
+  return `${list.slice(0, -1).join(', ')}, and ${list[list.length - 1]}`;
+}
