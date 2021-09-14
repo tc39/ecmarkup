74af9a36f30fab89572910525144b0d7299bf9e3
Kevin Gibbons <bakkot@gmail.com>
2021-09-13 18:26:05 -0700

    use a github action to deploy docs, instead of commiting files

---
+++ scripts/auto-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+declare -r GH_USER_EMAIL="bot@tc39"
+declare -r GH_USER_NAME="Bot"
+declare -r COMMIT_MESSAGE="Update gh-pages"
+
+cd "$(dirname "$BASH_SOURCE")"/../docs
+
+git config --global user.email "${GH_USER_EMAIL}"
+git config --global user.name "${GH_USER_NAME}"
+git config --global init.defaultBranch gh-pages
+git init
+git add -A
+git commit --message "${COMMIT_MESSAGE}"
+git push --force "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
