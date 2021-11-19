d216da5b6ae94123626b570708d7d5120353feae
Kevin Gibbons <kevin@shapesecurity.com>
2021-11-19 13:29:19 -0800

    fix typo

--- src/Meta.ts
+++ src/Meta.ts
@@ -12,7 +12,7 @@ export default class Meta extends Builder {
 
   static render(spec: Spec, node: HTMLElement) {
     // This builder turns <emu-meta> tags that aren't removed during effect
-    // propagation on invocations into <spans> so they are rendered.
+    // propagation on invocations into <span>s so they are rendered.
     if (node.hasAttribute('effects')) {
       const classNames = node
         .getAttribute('effects')!
