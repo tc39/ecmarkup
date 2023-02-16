a8c91afaef70bc959ed8779333f555837eb7f334
Kevin Gibbons <bakkot@gmail.com>
2023-02-16 09:14:23 -0800

    fix formatter

--- test/formatter.js
+++ test/formatter.js
@@ -474,6 +474,7 @@ describe('algorithm formatting', () => {
       1. Another   step.
             1. A substep.
             1. [ x =  "a",y="b"  ] A step with attributes.
+            1. A step with _vars_, numbers like **10<sup>x</sup>**, records like {  [[A]]: 0  }, and fields like _o_.[[field]].
       </emu-alg>
       `,
       dedentKeepingTrailingNewline`
@@ -482,6 +483,7 @@ describe('algorithm formatting', () => {
         1. Another step.
           1. A substep.
           1. [x="a", y="b"] A step with attributes.
+          1. A step with _vars_, numbers like **10<sup>x</sup>**, records like { [[A]]: 0 }, and fields like _o_.[[field]].
       </emu-alg>
       `
     );
