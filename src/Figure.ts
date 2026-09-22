c5f6efc596c14f52f3aa7b1836049a71c53112aa
Aki 🌹 <a@expertzebra.com>
2026-09-22 18:28:26 +0200

    Add figure numbering earlier in the constructor to maintain xrefs.

--- src/Figure.ts
+++ src/Figure.ts
@@ -5,7 +5,7 @@ import Builder from './Builder';
 
 export default class Figure extends Builder {
   type: string;
-  number: number;
+  number: number | string;
   id: string | null;
   isInformative: boolean;
   captionElem: HTMLElement | null;
@@ -16,12 +16,12 @@ export default class Figure extends Builder {
   constructor(spec: Spec, node: HTMLElement) {
     super(spec, node);
     this.type = node.nodeName.split('-')[1].toLowerCase();
-    this.number = ++spec._figureCounts[this.type];
+    this.number = `${spec._figurePrefix}${++spec._figureCounts[this.type]}`;
     this.id = node.getAttribute('id');
 
     this.isInformative = node.hasAttribute('informative');
     this.captionElem = node.querySelector('emu-caption');
-    this.caption = `${this.type.charAt(0).toUpperCase()}${this.type.slice(1)} ${spec._figurePrefix}${this.number}`;
+    this.caption = `${this.type.charAt(0).toUpperCase()}${this.type.slice(1)} ${this.number}`;
 
     if (this.isInformative) {
       this.caption += ' (Informative)';
@@ -61,10 +61,11 @@ export default class Figure extends Builder {
     const captionElem = spec.doc.createElement('figcaption');
     captionElem.innerHTML = figure.caption;
 
-    if (node.nodeName.split('-')[1] === 'FIGURE') {
-      node.firstElementChild?.appendChild(captionElem);
+    // Captions go below figures but above tables etc. (per Ecma house style)
+    if (node.nodeName === 'EMU-FIGURE') {
+      ele.appendChild(captionElem);
     } else {
-      node.childNodes[0].insertBefore(captionElem, node.childNodes[0].firstChild);
+      ele.insertBefore(captionElem, ele.firstChild);
     }
   }
 }
